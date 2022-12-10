427794fd7350ab589adc985f69e3a6342fbf498d
greembeems <hea4002@g.rit.edu>
2022-12-10 12:19:54 -0500

    All notes ready for testing
    Fix weird servo issue

--- project-3/project-3.ino
+++ project-3/project-3.ino
@@ -15,6 +15,9 @@ long speed = 0.2;
 int maxDegrees = 90;
 int minDegrees = 0; 
 
+// Speaker
+unsigned int speakerPin = 0;
+
 // Time
 unsigned long deltaTime = 0;
 unsigned long pastTime = 0;
@@ -41,10 +44,15 @@ void loop() {
     //servo.write(currentDegrees);
     PlayNote(inputValue);
   }
+  else
+  {
+    noTone(speakerPin);
+  }
 
   delay(3);
 }
 
+// Calculate delta time
 unsigned long DeltaTime()
 {
   unsigned long currentTime = millis();
@@ -52,7 +60,6 @@ unsigned long DeltaTime()
   pastTime = currentTime;
   // Return the delta time value
   return temp;
-  
 }
 
 // Play associated note for all 26 letters
@@ -62,108 +69,108 @@ void PlayNote(char key)
   // Line 1
   if (key == 'q')
   {
-    
+    tone(speakerPin, NOTE_B0);
   }
   else if (key == 'w')
   {
-    
+    tone(speakerPin, NOTE_C1);
   }
   else if (key == 'e')
   {
-    
+    tone(speakerPin, NOTE_D1);
   }
   else if (key == 'r')
   {
-    
+    tone(speakerPin, NOTE_E1);
   }
   else if (key == 't')
   {
-    
+    tone(speakerPin, NOTE_F1);
   }
   else if (key == 'y')
   {
-    
+    tone(speakerPin, NOTE_G1);
   }
   else if (key == 'u')
   {
-    
+    tone(speakerPin, NOTE_A1);
   }
   else if (key == 'i')
   {
-    
+    tone(speakerPin, NOTE_B1);
   }
   else if (key == 'o')
   {
-    
+    tone(speakerPin, NOTE_C2);
   }
   else if (key == 'p')
   {
-    
+    tone(speakerPin, NOTE_D2);
   }
   // Line 2
   else if (key == 'a')
   {
-    
+    tone(speakerPin, NOTE_E2);
   }
   else if (key == 's')
   {
-    
+    tone(speakerPin, NOTE_F2);
   }
   else if (key == 'd')
   {
-    
+    tone(speakerPin, NOTE_G2);
   }
   else if (key == 'f')
   {
-    
+    tone(speakerPin, NOTE_A2);
   }
   else if (key == 'g')
   {
-    
+    tone(speakerPin, NOTE_B2);
   }
   else if (key == 'h')
   {
-    
+    tone(speakerPin, NOTE_C3);
   }
   else if (key == 'j')
   {
-    
+    tone(speakerPin, NOTE_D3);
   }
   else if (key == 'k')
   {
-    
+    tone(speakerPin, NOTE_E3);
   }
   else if (key == 'l')
   {
-    
+    tone(speakerPin, NOTE_F3);
   }
   // Line 3
   else if (key == 'z')
   {
-    
+    tone(speakerPin, NOTE_G3);
   }
   else if (key == 'x')
   {
-    
+    tone(speakerPin, NOTE_A3);
   }
   else if (key == 'c')
   {
-    
+    tone(speakerPin, NOTE_B3);
   }
   else if (key == 'v')
   {
-    
+    tone(speakerPin, NOTE_C4);
   }
   else if (key == 'b')
   {
-    
+    tone(speakerPin, NOTE_D4);
   }
   else if (key == 'n')
   {
-    
+    tone(speakerPin, NOTE_F4);
   }
   else if (key == 'm')
   {
-    
+    tone(speakerPin, NOTE_G4);
   }
 }
