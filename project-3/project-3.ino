8d23a67849db162752420eca13b9aa36e91dd457
greembeems <hea4002@g.rit.edu>
2022-12-11 16:46:56 -0500

    speaker now working

--- project-3/project-3.ino
+++ project-3/project-3.ino
@@ -17,7 +17,7 @@ int minDegrees = 0;
 Servo servo;
 
 // Speaker
-unsigned int speakerPin = 0;
+unsigned int speakerPin = 9;
 
 // Time
 unsigned long deltaTime = 0;
@@ -25,7 +25,7 @@ unsigned long pastTime = 0;
 
 void setup() {
   // Attach the two motors
-  servo.attach(2, 530, 2600);
+  servo.attach(3, 530, 2600);
 
   Serial.begin(9600);
 }
@@ -37,7 +37,6 @@ void loop() {
     
     if (PlayNote(inputValue))
     {
-      Serial.print("Played note");
       // Reverse speed if out of range
       if (currentDegrees >= maxDegrees || currentDegrees <= minDegrees)
       {
@@ -45,10 +44,12 @@ void loop() {
       }
   
       currentDegrees = currentDegrees + speed * deltaTime;
-      servo.write(currentDegrees);
+      Serial.print("Current Degrees ");
+      Serial.print(currentDegrees + "\n");
+      servo.write(10);
     }
 
-  delay(3);
+  delay(0.02);
 }
 
 // Calculate delta time
@@ -67,141 +68,140 @@ bool PlayNote(String key)
 {
   Serial.print(key);
   // Line 1
-  if (key == "q")
+  if (key == "q\n")
   {
     tone(speakerPin, NOTE_B0);
     return true;
   }
-  else if (key == "w")
+  else if (key == "w\n")
   {
     tone(speakerPin, NOTE_C1);
     return true;
   }
-  else if (key == "e")
+  else if (key == "e\n")
   {
     tone(speakerPin, NOTE_D1);
     return true;
   }
-  else if (key == "r")
+  else if (key == "r\n")
   {
     tone(speakerPin, NOTE_E1);
     return true;
   }
-  else if (key == "t")
+  else if (key == "t\n")
   {
     tone(speakerPin, NOTE_F1);
     return true;
   }
-  else if (key == "y")
+  else if (key == "y\n")
   {
     tone(speakerPin, NOTE_G1);
     return true;
   }
-  else if (key == "u")
+  else if (key == "u\n")
   {
     tone(speakerPin, NOTE_A1);
     return true;
   }
-  else if (key == "i")
+  else if (key == "i\n")
   {
     tone(speakerPin, NOTE_B1);
     return true;
   }
-  else if (key == "o")
+  else if (key == "o\n")
   {
     tone(speakerPin, NOTE_C2);
     return true;
   }
-  else if (key == "p")
+  else if (key == "p\n")
   {
     tone(speakerPin, NOTE_D2);
     return true;
   }
   // Line 2
-  else if (key == "a")
+  else if (key == "a\n")
   {
     tone(speakerPin, NOTE_E2);
     return true;
   }
-  else if (key == "s")
+  else if (key == "s\n")
   {
     tone(speakerPin, NOTE_F2);
     return true;
   }
-  else if (key == "d")
+  else if (key == "d\n")
   {
     tone(speakerPin, NOTE_G2);
     return true;
   }
-  else if (key == "f")
+  else if (key == "f\n")
   {
     tone(speakerPin, NOTE_A2);
     return true;
   }
-  else if (key == "g")
+  else if (key == "g\n")
   {
     tone(speakerPin, NOTE_B2);
     return true;
   }
-  else if (key == "h")
+  else if (key == "h\n")
   {
     tone(speakerPin, NOTE_C3);
     return true;
   }
-  else if (key == "j")
+  else if (key == "j\n")
   {
     tone(speakerPin, NOTE_D3);
     return true;
   }
-  else if (key == "k")
+  else if (key == "k\n")
   {
     tone(speakerPin, NOTE_E3);
     return true;
   }
-  else if (key == "l")
+  else if (key == "l\n")
   {
     tone(speakerPin, NOTE_F3);
     return true;
   }
   // Line 3
-  else if (key == "z")
+  else if (key == "z\n")
   {
     tone(speakerPin, NOTE_G3);
     return true;
   }
-  else if (key == "x")
+  else if (key == "x\n")
   {
     tone(speakerPin, NOTE_A3);
     return true;
   }
-  else if (key == "c")
+  else if (key == "c\n")
   {
     tone(speakerPin, NOTE_B3);
     return true;
   }
-  else if (key == "v")
+  else if (key == "v\n")
   {
     tone(speakerPin, NOTE_C4);
     return true;
   }
-  else if (key == "b")
+  else if (key == "b\n")
   {
     tone(speakerPin, NOTE_D4);
     return true;
   }
-  else if (key == "n")
+  else if (key == "n\n")
   {
     tone(speakerPin, NOTE_F4);
     return true;
   }
-  else if (key == "m")
+  else if (key == "m\n")
   {
     tone(speakerPin, NOTE_G4);
     return true;
   }
   else 
   {
-    Serial.print("Did not play note\n");
     noTone(speakerPin);
     return false;
   }
